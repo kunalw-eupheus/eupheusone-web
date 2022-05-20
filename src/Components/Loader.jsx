e774083643a32393cd546c0069b3d228dcf11b66
adeshEupheus <104565905+adeshEupheus@users.noreply.github.com>
2022-05-20 12:33:20 +0530

    commit

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
