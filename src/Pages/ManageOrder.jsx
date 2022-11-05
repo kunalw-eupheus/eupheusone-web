22023fa5de854a8863cbdad5a29af830e874663a
adeshshrma <me@mydomail.com>
2022-11-06 01:00:31 +0530

    Css Minimizer plugin issue

--- src/Pages/ManageOrder.jsx
+++ src/Pages/ManageOrder.jsx
@@ -1,86 +1,86 @@
-import React, { useEffect, useRef } from "react";
-import { useState } from "react";
-import Navbar from "../Components/Navbar";
-import Sidebar from "../Components/Sidebar";
+import React, { useEffect, useRef } from 'react'
+import { useState } from 'react'
+import Navbar from '../Components/Navbar'
+import Sidebar from '../Components/Sidebar'
 // import { Add } from "@mui/icons-material";
 // import { Link } from "react-router-dom";
 // import DataTable from "../Components/DataTable";
 // import { rows, ManageSchoolRows } from "../DummyData";
 // import SearchDropDown from "../Components/SearchDropDown";
-import SwipeableTemporaryDrawer from "../Components/Material/MaterialSidebar";
-import instance from "../Instance";
-import { useLayoutEffect } from "react";
-import Cookies from "js-cookie";
+import SwipeableTemporaryDrawer from '../Components/Material/MaterialSidebar'
+import instance from '../Instance'
+import { useLayoutEffect } from 'react'
+import Cookies from 'js-cookie'
 
 const ManageOrder = () => {
-  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
-  const [highLight, setHighLight] = useState("manageOrder");
-  const [orderData, setOrderData] = useState([]);
-  const [orderNum, setOrderNum] = useState(0);
-  const sidebarRef = useRef();
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
+  const [highLight, setHighLight] = useState('manageOrder')
+  const [orderData, setOrderData] = useState([])
+  const [orderNum, setOrderNum] = useState(0)
+  const sidebarRef = useRef()
 
   const navInfo = {
-    title: "Manage Order",
-    details: ["Home", " / Manage Order"],
-  };
+    title: 'Manage Order',
+    details: ['Home', ' / Manage Order'],
+  }
 
   const Tablecolumns = [
-    { field: "SchoolName", headerName: "School Name", width: 300 },
+    { field: 'SchoolName', headerName: 'School Name', width: 300 },
     {
-      field: "City",
-      headerName: "City",
+      field: 'City',
+      headerName: 'City',
       width: 180,
     },
     {
-      field: "State",
-      headerName: "State",
+      field: 'State',
+      headerName: 'State',
       width: 120,
     },
     {
-      field: "Address",
-      headerName: "Address",
+      field: 'Address',
+      headerName: 'Address',
       width: 350,
     },
-  ];
+  ]
 
   const handleSidebarCollapsed = () => {
     // setSidebarCollapsed(!sidebarCollapsed);
-    sidebarRef.current.openSidebar();
-  };
+    sidebarRef.current.openSidebar()
+  }
 
   useLayoutEffect(() => {
     const getOrderData = async () => {
       const data = await instance({
-        url: "sales_data/get/orders",
-        method: "GET",
+        url: 'sales_data/get/orders',
+        method: 'GET',
         headers: {
-          Authorization: Cookies.get("accessToken"),
+          Authorization: Cookies.get('accessToken'),
         },
-      });
-      setOrderNum(data.data.message.length);
-      setOrderData(data.data.message);
+      })
+      setOrderNum(data.data.message.length)
+      setOrderData(data.data.message)
       // console.log(data.data.message);
-    };
-    getOrderData();
-  }, []);
+    }
+    getOrderData()
+  }, [])
 
   useEffect(() => {
     const handleWidth = () => {
       if (window.innerWidth > 1024) {
-        setSidebarCollapsed(false);
+        setSidebarCollapsed(false)
       } else {
-        setSidebarCollapsed(true);
+        setSidebarCollapsed(true)
       }
-    };
-    window.addEventListener("resize", handleWidth);
-    handleWidth();
-    window.scroll(0, 0);
+    }
+    window.addEventListener('resize', handleWidth)
+    handleWidth()
+    window.scroll(0, 0)
     return () => {
-      window.removeEventListener("resize", handleWidth);
-    };
-  }, []);
+      window.removeEventListener('resize', handleWidth)
+    }
+  }, [])
   return (
-    <div className="flex bg-[#111322]">
+    <div className='flex bg-[#111322]'>
       <Sidebar sidebarCollapsed={sidebarCollapsed} highLight={highLight} />
 
       <div>
@@ -94,34 +94,32 @@ const ManageOrder = () => {
 
       <div
         className={`flex flex-col w-[100vw] lg:w-[83vw] lg:ml-[18vw] ${
-          window.innerWidth < 1024 ? null : "md:ml-[30vw] ml-[60vw]"
+          window.innerWidth < 1024 ? null : 'md:ml-[30vw] ml-[60vw]'
         } `}
       >
         <Navbar
           handleSidebarCollapsed={handleSidebarCollapsed}
           info={navInfo}
         />
-        <div className="min-h-[100vh] pt-[2vh] max-h-full bg-[#141728]">
+        <div className='min-h-[100vh] pt-[2vh] max-h-full bg-[#141728]'>
           <div
-            className={`sm:px-8 px-4 py-3 grid sm:grid-cols-2 grid-cols-1 grid-rows-[${orderNum}] sm:grid-rows-[${
-              orderNum / 2
-            }] gap-4 bg-[#141728]`}
+            className={`sm:px-8 px-4 py-3 grid sm:grid-cols-2 grid-cols-1 gap-4 bg-[#141728]`}
           >
             {orderData.map((item) => {
               return (
-                <div className="flex font-medium flex-col gap-4 text-gray-100 justify-center rounded-md items-start px-4 py-1 bg-slate-600">
+                <div className='flex font-medium flex-col gap-4 text-gray-100 justify-center rounded-md items-start px-4 py-1 bg-slate-600'>
                   <span>Order Type: {item.order_type}</span>
                   <span>School Name: {item.fk_school.school_name}</span>
                   <span>Quantity: {item.quantity}</span>
                   <span>Amount: ₹{item.amount}</span>
                 </div>
-              );
+              )
             })}
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ManageOrder;
+export default ManageOrder
