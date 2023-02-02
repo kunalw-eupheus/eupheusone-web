7af523c8f682f35909ab20ddda619af805bbf69c
adeshEupheus <104565905+adeshEupheus@users.noreply.github.com>
2023-02-02 17:20:08 +0530

    ck training location added

--- src/Instance.js
+++ src/Instance.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
 
  
-    baseURL: "https://api.eupheusapp.com/api/",
-  // baseURL: "https://stageapi.eupheusapp.com/api/",
+  //  baseURL: "https://api.eupheusapp.com/api/",
+   baseURL: "https://stageapi.eupheusapp.com/api/",
 
 });
 
