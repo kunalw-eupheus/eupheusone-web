7f5058d58863e1806f40590f6bbc205dd631c225
Bidipta Bikash Gogoi <bidiptag@eupheus.in>
2023-04-21 17:05:52 +0530

    gatepass login corrected

--- src/localinstance.js
+++ src/localinstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://api.eupheusapp.com/api/",
-  // baseURL: "https://stageapi.eupheusapp.com/api/",
-  baseURL: "http://192.168.7.148:5070/api/",
+  baseURL: "https://stageapi.eupheusapp.com/api/",
+  // baseURL: "http://192.168.7.148:5070/api/",
 });
 
 export default instance;
