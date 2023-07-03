7643b2fb1658f69a99e83c532c2e64b65a4c8b62
adesh sharma <adeshs@eupheus.in>
2023-07-03 16:33:52 +0530

    added collection xl

--- src/Instance.js
+++ src/Instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.eupheusapp.com/api/",
-  // baseURL: "https://stageapi.eupheusapp.com/api/",
+  // baseURL: "https://api.eupheusapp.com/api/",
+  baseURL: "https://stageapi.eupheusapp.com/api/",
   // baseURL: "http://192.168.7.168:5070/api/",
   // baseURL: "http://192.168.7.55:5070/api/",
 });
