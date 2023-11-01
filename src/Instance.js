8ab8b05a1871aa74eb0b9c775445d080f4edf4d2
Your Name <you@example.com>
2023-11-01 16:29:35 +0530

    firebase

--- src/Instance.js
+++ src/Instance.js
@@ -3,8 +3,8 @@ import { errorActions } from "./Store/error";
 import store from "./Store";
 
 const instance = axios.create({
-  baseURL: "https://api.eupheusapp.com/api/",
-  // baseURL: "https://stageapi.eupheusapp.com/api/",
+  // baseURL: "https://api.eupheusapp.com/api/",
+  baseURL: "https://stageapi.eupheusapp.com/api/",
   // baseURL: "http://192.168.7.168:5070/api/",
   // baseURL: "http://192.168.7.55:5070/api/",
 });
